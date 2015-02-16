8f197453be5384606acd27266f1b5c38fa5b29d9
Daniel Kirov <daniel.kirov@pancentric.com>
2015-02-16 11:35:09 +0000

    Updated django-appconf version to 1.0.1 since 0.6 is not available anymore.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "django-classy-tags==0.4",
         "twitter==1.9.1",
-        "django-appconf==0.6",
+        "django-appconf==1.0.1",
         'six==1.2.0',
         # 'django-cacheback==0.7',
     ],
