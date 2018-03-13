323853421217f859e8b7fbf6f901bef09333657d
Mo Shami <mo.shami@pancentric.com>
2018-03-13 11:14:26 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ from setuptools import setup, find_packages
 read = lambda filepath: codecs.open(filepath, 'r', 'utf-8').read()
 
 tests_require = [
-    'Django>=1.2',
-    'nose==1.2.1',
-    'mock==1.0.1',
-    'httpretty==0.5.9',
-    'sure==1.1.7',
+    'Django==1.11.11',
+    'nose==1.3.7',
+    'mock==2.0.0',
+    'httpretty==0.8.14',
+    'sure==1.4.7',
     'coveralls',
 ]
 
 setup(
     name='django-twitter-tag',
-    version='1.2',
+    version='1.1',
     author='Ilya Baryshev',
     author_email='baryshev@gmail.com',
     packages=find_packages(exclude="tests"),
@@ -24,11 +24,10 @@ setup(
     description="A django template tag to display user's recent tweets.",
     long_description=read(path.join(path.dirname(__file__), 'README.rst')),
     install_requires=[
-        "django-classy-tags==0.4",
-        "twitter==1.9.1",
-        "django-appconf==1.0.1",
-        'six==1.2.0',
-        # 'django-cacheback==0.7',
+        "django-classy-tags==0.8.0",
+        "twitter==1.18.0",
+        "django-appconf==1.0.2",
+        'six==1.11.0',
     ],
     tests_require=tests_require,
     test_suite="nose.collector",
